b6867391a8fed8464f14776e2a4324d5e069abdd
Melquiades998 <69167799+Melquiades998@users.noreply.github.com>
2025-06-07 12:35:37 -0400

    fix CORS

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -35,7 +35,11 @@ export const POST: APIRoute = async ({request}) => {
       {
         status: 500,
         headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST',
+          'Access-Control-Allow-Headers': 'Content-Type', 
           'Content-Type': 'application/json',
+
         },
       }
     );
